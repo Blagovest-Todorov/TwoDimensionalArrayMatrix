9c3d693f1d86b9af438e747eebf7f04f18333aba
Blagovest-Todorov <blagovest.stod@abv.bg>
2021-04-12 09:14:40 +0300

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MultidimensionalArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = 
+            {
+                { 1, 2, 3, 4},
+                { 5, 6 , 7, 8}
+            };
+
+            Console.WriteLine(matrix.GetLength(0)); // We take the numbs of rows
+            Console.WriteLine(matrix.GetLength(1)); // we take the numbs of cols
+        }
+    }
+}
